013dc7cf2f0a1463f0cb9352a27af3d9f8cf0062
Alejandro Goiochea <alejandrogoicochea@gmail.com>
2019-08-21 17:43:03 -0500

    chore(Sorting class): Added sorting class with unordered vector and methods.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include<vector>
+
+using namespace std;
+
+template<typename T>
+class Sorting{
+private:
+    vector<T> myVector = {7,3,5,9,3,14,62,4,25,4562,36};
+public:
+    Sorting()= default;
+
+    void InsertSort(){};
+    void SelectionSort(){};
+    void BubbleSort(){};
+    void MergeSort(){};
+    void HeapSort(){};
+    void QuickSort(){};
+
+};
+
 
 int main (int, char * []){
+
+    Sorting<int> mySort;
+
+    mySort.InsertSort();
+    mySort.SelectionSort();
+    mySort.BubbleSort();
+    mySort.MergeSort();
+    mySort.HeapSort();
+    mySort.QuickSort();
+
     std::cout << "Hello World" << std::endl;
     return 1;
 }
