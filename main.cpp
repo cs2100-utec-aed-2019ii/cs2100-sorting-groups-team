a2046a0b4fe88f4acfde4cad2e5f7ac47aeb4555
Alejandro Goiochea <alejandrogoicochea@gmail.com>
2019-08-21 18:01:52 -0500

    feat(InsertSort):InsertSort finished with print.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,7 +11,24 @@ private:
 public:
     Sorting()= default;
 
-    void InsertSort(){};
+    void InsertSort(){
+
+        unsigned int j;
+
+        vector<T> a = {7,3,5,9,3,14,62,4,25,4562,36};
+        for(unsigned int i = 1; i < a.size();i++){
+            j = i;
+            while(j>0 && a[j-1]>a[j]){
+                swap(a[j],a[j-1]);
+                j--;
+            }
+        }
+
+        for(T i: a){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    };
     void SelectionSort(){};
     void BubbleSort(){};
     void MergeSort(){};
