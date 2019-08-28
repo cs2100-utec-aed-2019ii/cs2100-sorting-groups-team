0ef93fd0ace421195713b983de1c62ee1b174554
Gonzalo <gonzalo.alfacaso@gmail.com>
2019-08-28 15:22:14 -0500

    CountingSort elimiated

--- main.cpp
+++ main.cpp
@@ -258,37 +258,6 @@ public:
 			cout<<endl;
 		};
 
-		void CountingSort()
-		{
-			auto a = myVector;
-			int max = *max_element(a.begin(), a.end());
-			int min = *min_element(a.begin(), a.end());
-			int range = max - min + 1;
-
-			vector<int> count(range), output(a.size());
-
-			for(int i = 0; i < a.size(); i++)
-				count[a[i]-min]++;
-
-			for(int i = 1; i < count.size(); i++)
-				count[i] += count[i-1];
-			
-			for(int i = a.size()-1; i >= 0; i--)
-			{
-				output[count[a[i]-min] - 1] = a[i];
-				count[a[i]-min]--;
-			}
-
-			for(int i = 0; i < a.size(); i++)
-				a[i] = output[i];
-
-			cout<<"CountingSort"<<endl;
-			for(const auto& i: a)
-				cout<<i<<" ";
-			
-			cout<<endl;
-		}
-
 };
 
 
@@ -303,7 +272,6 @@ int main (int, char * []){
     mySort.HeapSort();
     mySort.QuickSort();
     mySort.BrickSort();
-		mySort.CountingSort();
 
 	return 1;
 }
